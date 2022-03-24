c1388dad17af4d5752053fb02bc4d888f0ae8eaa
Luis Corales <lcorales@stackbuilders.com>
2022-03-24 12:26:05 -0500

    Get repos data and sort it

--- index.js
+++ index.js
@@ -4,13 +4,50 @@
  */
 
 import fetch from "node-fetch";
+import fs from "fs";
 
 const url = "https://api.github.com/orgs/stackbuilders/repos";
 
-const fetchData = (url) => {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => console.log(data));
+// Fetch the API url
+const fetchData = async (url) => {
+    const response = await fetch(url);
+    const jsonData = await response.json();
+
+    return jsonData;
 };
 
-fetchData(url);
+// Get the stars sum of all repos
+const getStarsSum = (data) => {
+    return data.reduce((sum, repo) => sum + repo.stars, 0);
+};
+
+// Get the 5 latest updated repos
+const sortReposByUpdateDate = (data) => {
+    return data.sort((a, b) => {
+        return new Date(b.updated_at) - new Date(a.updated_at);
+    }).slice(0, 5);
+};
+
+// From the GitHub API response, get the needed data
+const getRepoData = async (url) => {
+    const data = await fetchData(url);
+    return data.map(repo => ({
+        repo_name: repo.full_name.split("/")[1], 
+        url: repo.html_url, 
+        updated_at: repo.updated_at, 
+        stars: repo.stargazers_count
+    }));
+};
+
+(async () => {
+    const data = await getRepoData(url);
+
+    const wholeDataText = JSON.stringify(data);
+    fs.writeFileSync('./data/whole_data.json', wholeDataText);
+
+    const updatedRecentlyText = JSON.stringify(sortReposByUpdateDate(data));
+    fs.writeFileSync('./data/sorted_by_update_data.json', updatedRecentlyText);
+
+
+    console.log(sortReposByUpdateDate(data));
+})();
